c31beaa783fd56d91a50bf8740381c7db7dfaf90
Henning Voß <henning@huhehu.com>
2024-10-01 16:19:32 +0200

    refactor: move sleep before shutdown duration time to server config

--- config/config.go
+++ config/config.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Conf struct {
-	Server                      ConfServer
-	AirPollutionFile            string `env:"AIR_POLLUTION_FILE,default=air-pollution.csv"`
-	SleepDurationBeforeShutdown string `env:"SLEEP_DURATION_BEFORE_SHUTDOWN,default=10s"`
+	Server           ConfServer
+	AirPollutionFile string `env:"AIR_POLLUTION_FILE,default=air-pollution.csv"`
 }
 
 type ConfServer struct {
-	Port int `env:"SERVER_PORT,default=8080"`
+	Port                        int    `env:"SERVER_PORT,default=8080"`
+	SleepDurationBeforeShutdown string `env:"SLEEP_DURATION_BEFORE_SHUTDOWN,default=10s"`
 }
 
 func New() *Conf {
